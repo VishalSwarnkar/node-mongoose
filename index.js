41043ee189da887f1770a6ac776ec9b1f332ef61
VishalSwarnkar <vishal.swa@gmail.com>
2017-10-29 07:04:00 +0000

    node with mongoose

---
+++ index.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+mongoose.Promises = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confusion';
+
+const connect = mongoose.connect(url, {useMongoClient: true})
+
+
+connect.then((db)=>{
+  console.log('Connected correctly to the server');
+
+  var newDish = Dishes({
+    name: 'Vishal',
+    description: 'test'
+  })
+
+  newDish.save()
+  .then((dish)=>{
+    console.log(dish);
+    return Dishes.find({}).exec();
+  })
+  .then((Dishes)=>{
+    console.log(Dishes);
+    return db.collection('dishes').drop();
+  })
+  .then(()=>{
+    return db.close();
+  })
+  .catch((error)=>{
+    console.log(error);
+  })
+});
