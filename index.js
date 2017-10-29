3123f1e0612f60084ef55b1b3c18ab4cd156ae90
VishalSwarnkar <vishal.swa@gmail.com>
2017-10-29 07:34:21 +0000

    mongoose part 3

--- index.js
+++ index.js
@@ -17,10 +17,25 @@ connect.then((db)=>{
   })
   .then((dish)=>{
     console.log(dish);
-    return Dishes.find({}).exec();
+    return Dishes.findByIdAndUpdate(dish._id,{
+      $set: {description: 'Updated test'}
+    },{
+      new: true
+    })
+    .exec();
   })
-  .then((Dishes)=>{
-    console.log(Dishes);
+  .then((dish)=>{
+    console.log(dish);
+
+    dish.comments.push({
+      rating: 5,
+      comment: 'I exicited about mongoose db',
+      author: 'Leonardo'
+    });
+    return dish.save();
+  })
+  .then((dish)=>{
+    console.log(dish);
     return db.collection('dishes').drop();
   })
   .then(()=>{
